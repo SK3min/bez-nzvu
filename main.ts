759ddb2664e7404b46f9d6e42f707d545df8a040
SK3min <adrian.gonda21@gmail.com>
2025-05-11 15:05:28 +0200

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,54 @@
+function gg (LOVE_YOU: string, SRDCE: Image) {
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+}
+basic.forever(function () {
+	
+})
